603101cd660db3dc1a0db658439b9f1a9f1bce79
kvbqq <kvbqqq@gmail.com>
2026-01-20 17:53:33 +0100

    Initial commit: Add Spring Boot project structure

---
+++ src/test/java/com/safestay/safe_stay_backend/SafeStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safestay.safe_stay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
