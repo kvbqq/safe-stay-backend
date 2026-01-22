cb9238589e46ec104b5b14152809670cc5c7acf4
kvbqq <kvbqqq@gmail.com>
2026-01-22 22:39:01 +0100

    api: add UserController with registration endpoint

---
+++ src/main/java/com/safestay/safe_stay_backend/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.safestay.safe_stay_backend.controller;
+
+import com.safestay.safe_stay_backend.dto.UserRegistrationDTO;
+import com.safestay.safe_stay_backend.dto.UserResponseDTO;
+import com.safestay.safe_stay_backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public UserResponseDTO registerUser(@RequestBody UserRegistrationDTO registrationDTO) {
+        return userService.registerUser(registrationDTO);
+    }
+}
